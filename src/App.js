88c15b6bcd3ae0abfd05f90c7ec94a935dbde936
HungNguyenBa1811 <nahh1811@gmail.com>
2023-08-28 20:21:53 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import TodoList from './components/TodoList';
 
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 
 function App() {
   return (
@@ -13,10 +13,13 @@ function App() {
         <Col lg={3} md={2} sm={2} className='blank'></Col>
         <Col lg={6} md={8} sm={8} className='mainApp'>
           <div className='task_render'>
-            <TodoList order={1}/>    
-            <TodoList order={2}/>    
-            <TodoList order={3}/>    
-            <TodoList order={4}/>    
+            <TodoList order={1}/>
+            <TodoList order={2}/>
+            <TodoList order={3}/>
+            <TodoList order={4}/>
+            <Button className='btn-circle'>
+              <i className='fa fa-plus'></i>
+            </Button>
           </div>
         </Col>
         <Col lg={3} md={2} sm={2} className='blank'></Col>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import "https://pro.fontawesome.com/releases/v5.15.4/css/all.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
